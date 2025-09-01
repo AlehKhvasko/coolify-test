54521b0d9da37941d0b730b22654dd59f1e80891
AlehKhvasko <57001092+AlehKhvasko@users.noreply.github.com>
2025-08-31 20:45:46 -0500

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello Coolify CI/CD 🚀");
+  res.send("Pipe test #1");
 });
 
 app.listen(PORT, () => {
