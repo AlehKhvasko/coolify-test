afe3a93e3a9cb4920476306d7b1b9acb7ea3028e
AlehKhvasko <57001092+AlehKhvasko@users.noreply.github.com>
2025-08-31 21:51:10 -0500

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send("Pipe test #15");
+  res.send("Pipe test #16");
 });
 
 app.listen(PORT, () => {
